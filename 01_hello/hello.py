2ba9fa56f4da5326bb222c6f4bba929baed79258
Peter Dosev <pdosev@gmail.com>
2020-11-10 22:29:09 -0500

    Make the linter feel happy

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -2,12 +2,22 @@
 
 import argparse
 
-def main():
+
+def argument_parse():
+	"""Parse arguments *le gasp* """
+
+
 	parser = argparse.ArgumentParser(description="come to the forbidden apple")
 	parser.add_argument('-n', '--name', metavar='name', default='World', help="your own identifier")
-	args = parser.parse_args()
+	return parser.parse_args()
 
+def main():
+	"""This is where the magic happens"""
+
+
+	args = argument_parse()
 	print ("Hello, " + args.name + "!")
+
 
 if __name__ == '__main__':
 	main()
