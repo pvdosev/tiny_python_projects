409a2608ccc696f812917fd2ef9b18fd5b9828af
Various Artists <a.jolly.good.time>
2020-11-12 12:59:38 -0500

    Linted 03, completed solution for 04

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -3,44 +3,39 @@
 
 import argparse
 
+
 def get_args():
-	"""Are you tired of arguments yet? Cause there'll be way more"""
-	parser = argparse.ArgumentParser(
-		description="What are we bringing, boss?",
-		formatter_class=argparse.ArgumentDefaultsHelpFormatter
-		)
-	parser.add_argument(
-		"items",
-		metavar="item",
-		nargs="+",
-		help="Thing(s) a picnic needs"
-		)
-	parser.add_argument(
-		"-s",
-		"--sorted",
-		help = "Sort the list?",
-		action = "store_true"
-		)
-	return parser.parse_args()
+    """Are you tired of arguments yet? Cause there'll be way more"""
+    parser = argparse.ArgumentParser(
+        description="What are we bringing, boss?",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "items", metavar="item", nargs="+", help="Thing(s) a picnic needs"
+    )
+    parser.add_argument("-s", "--sorted", help="Sort the list?", action="store_true")
+    return parser.parse_args()
+
 
 def main():
-	"""Make sure to bring the waterproof blanket"""
-	args = get_args()
-	commaCounter = 1
-	
-	if args.sorted:
-		args.items.sort()
+    """Make sure to bring the waterproof blanket"""
+    args = get_args()
+    comma_counter = 1
 
-	if len(args.items) == 1:
-		print(f"You are bringing {args.items[0]}.")
-	elif len(args.items) == 2:
-		print(f"You are bringing {args.items[0]} and {args.items[1]}.")
-	elif len(args.items) > 2:
-		for x in range(len(args.items) - 2):
-			args.items.insert(commaCounter, ", ")
-			commaCounter = commaCounter + 2
-		args.items.insert(-1, ", and ")
-		print(f"You are bringing {''.join(args.items)}.")
+    if args.sorted:
+        args.items.sort()
 
-if __name__ == '__main__':
-	main()
+    if len(args.items) == 1:
+        print(f"You are bringing {args.items[0]}.")
+    elif len(args.items) == 2:
+        print(f"You are bringing {args.items[0]} and {args.items[1]}.")
+    elif len(args.items) > 2:
+        for x in range(len(args.items) - 2):
+            args.items.insert(comma_counter, ", ")
+            comma_counter = comma_counter + 2
+        args.items.insert(-1, ", and ")
+        print(f"You are bringing {''.join(args.items)}.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""This is based off of some popular crime show, I hear"""
+
+import argparse
+
+
+def get_args():
+    """Argumentation documentation"""
+    parser = argparse.ArgumentParser(
+        description="Ayy, I'm typin' heah!",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "string", metavar="string", help="Hey kid, wanna scramble some numbers?"
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Don't mind me, just moving around some numbers"""
+    args = get_args()
+    scrambled = ""
+    number_dict = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "5": "0",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+        "0": "5",
+    }
+
+    for char in args.string:
+        scrambled = scrambled + number_dict.get(char, char)
+
+    print(scrambled)
+
+
+if __name__ == "__main__":
+    main()
