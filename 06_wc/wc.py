8343063398e990337541911ee38f76158b9a17fa
Various Artists <a.jolly.good.time>
2020-11-16 12:41:05 -0500

    Complete solution for 06

--- 06_wc/wc.py
+++ 06_wc/wc.py
@@ -4,31 +4,34 @@
 import argparse
 import sys
 
+
 def get_args():
     """Arrr, yet more arrrgs"""
     parser = argparse.ArgumentParser(
-        description='Will Crusher',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Will Crusher",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='A file that counts!',
-                        nargs='*',
-                        metavar='file',
-                        type=argparse.FileType('rt'),
-                        default=[sys.stdin])
+    parser.add_argument(
+        "file",
+        help="A file that counts!",
+        nargs="*",
+        metavar="file",
+        type=argparse.FileType("rt"),
+        default=[sys.stdin],
+    )
 
     return parser.parse_args()
+
 
 def main():
     """Water closet, wand curse, woozy client"""
 
     args = get_args()
+
     total_lines = 0
     total_words = 0
     total_bytes = 0
-
-    """Check for args.file, otherwise use stdin. Turn the counting into a function?"""
 
     for fh in args.file:
         current_lines = 0
@@ -41,11 +44,11 @@ def main():
         total_lines += current_lines
         total_words += current_words
         total_bytes += current_bytes
-
         print(f"{current_lines:8}{current_words:8}{current_bytes:8} {fh.name}")
+
     if len(args.file) > 1:
         print(f"{total_lines:8}{total_words:8}{total_bytes:8} total")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
